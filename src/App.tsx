9367f13e4317f261442c4f7a8a06ed8554daa2da
sutigit <panyanilben@gmail.com>
2024-11-12 15:50:43 +0200

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css'
+import './App.css';
+import { useEffect, useRef } from 'react';
+import StatMapDisplay, { Country, AdministrativeLevel } from 'stat-map-display';
 
 function App() {
+  const viewRef = useRef<StatMapDisplay | null>(null);
+
+  useEffect(() => {
+    if (!viewRef.current) {
+      viewRef.current = new StatMapDisplay({
+        id: 'my-map',
+        country: Country.FINLAND,
+        administrativeLevel: AdministrativeLevel.MUNICIPALITY,
+        style: {
+          strokeWidth: 0.1,
+        },
+        settings: {
+          select: false,
+          highlight: false,
+        }
+      });
+    }
+  }, []);
 
   return (
-    <main>
-      <h1>Hello</h1>
+    <main id="app">
+      <div className="options">
+        <p>Hello</p>
+      </div>
+
+      <div className='map-container'>
+        <div id='my-map'></div>
+      </div>
     </main>
   )
 }
