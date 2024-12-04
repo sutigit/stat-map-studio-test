4f912265f93fb2292284967edd9ebe2b45112d84
sutigit <panyanilben@gmail.com>
2024-12-04 19:52:28 +0200

    add documentation

--- src/servers/MediaProcessorServer.ts
+++ src/servers/MediaProcessorServer.ts
@@ -1,5 +1,8 @@
 export default class MediaProcessorServer {
     /**
+     * This method sends a POST request to the stat-map-video-processing server to attempt to process it 
+     * for a video based on the attached timeseries data.
+     * It sends the maps_svg string, timesries data as tsdata, videoWidth, and videoHeight as the payload.
      * 
      * @param svgString
      * @param tsdata
